173f0b9d29c5575e09ce88e9d788dd1648cc4353
muzamilhussain33 <muzamilhussain369@gmail.com>
2026-02-09 17:46:50 +0500

    fix new bug

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/Custom-Youtube/', // Ensures assets load correctly on GitHub Pages
 })
